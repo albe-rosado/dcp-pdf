7618949266f72eeabe7f6dbd85f0b6a7ea2b72a4
Alberto Rosado <alberosado7@gmail.com>
2019-05-31 20:07:58 -0400

    initial auth boilerplate

---
+++ index.js
@@ -0,0 +1,15 @@
+const {google} = require('googleapis');
+cosnt _ = require('lodash');
+const GmailAuth = require('./gmail-auth');
+
+GmailAuth('credentials.json', processEmails);
+
+
+
+
+async function processEmails(auth) {
+    const Gmail = google.gmail({version: 'v1', auth});
+    const messages = await Gmail.users.messages.list({userId: 'me', q: 'from:founders@dailycodingproblem.com'});
+    console.log(messages)
+}
+
