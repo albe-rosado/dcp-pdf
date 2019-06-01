cef59711b71038d703d94701b47533abd61d7f05
Alberto Rosado <alberosado7@gmail.com>
2019-05-31 20:07:58 -0400

    initial auth boilerplate

---
+++ index.js
@@ -0,0 +1,13 @@
+const {google} = require('googleapis');
+
+const GmailAuth = require('./gmail-auth');
+
+GmailAuth('credentials.json', processEmails);
+
+
+
+
+async function processEmails(auth) {
+    
+}
+
